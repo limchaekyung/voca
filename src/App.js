c5318333a5117eb905bc0ad2bfc298ad11aba7c6
limchaekyung <corud505@naver.com>
2022-06-20 23:38:14 +0900

    module css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Hello from './component/Hello'
 import Welcome from './component/Welcome'
+import styles from './App.module.css'
 
 function App() {
   return (
     <div className="App">
       <Hello />
-      <Welcome />
+      {/* <Welcome /> */}
+      {/* <Hello /> */}
+      {/* <Hello /> */}
+      <div className={styles.box}>App</div>
     </div>
   );
 }
--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,10 +1,20 @@
 import World from "./World";
+import styles from "./Hello.module.css"
 
 export default function Hello() {
     return (
         <>
-            <p>Hello</p>
-            <World />
+            <h1
+                style={{
+                    color: '#f00',
+                    borderRight: '2px solid #000',
+                    marginBottom: '50px',
+                    opacity: 1,
+                }}
+            >Hello</h1>
+            {/* <World /> */}
+            {/* <World /> */}
+            <div className={styles.box}>Hello</div>
         </>
     )
 }
