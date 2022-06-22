f4c8226251116d0b2b82dfaade9ef24425ee9d3c
limchaekyung <corud505@naver.com>
2022-06-22 22:13:53 +0900

    react-route-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import Day from "./component/Day";
 import DayList from "./component/DayList";
 import Header from "./component/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import EmptyPage from "./component/EmptyPage";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <DayList />
-      <Day />
-
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<DayList />} />
+          <Route path="/day/:day" element={<Day />} />
+          <Route element={<EmptyPage />} />
+        </Routes>
+      </div >
+    </BrowserRouter >
   );
 }
 
